77d49012569a06ea641eff74d1c5bdb3ef9b898d
Zheng Guo <zhg069@eng.ucsd.edu>
2021-01-25 20:10:20 -0800

    update gitignore

---
+++ webapp/src/reducers/index.js
@@ -0,0 +1,13 @@
+import * as Consts from "../constants/action-types"
+
+const initialState = {
+
+};
+
+const rootReducer = (state = initialState, action) => {
+    switch(action.type) {
+
+    }
+}
+
+export default rootReducer;
