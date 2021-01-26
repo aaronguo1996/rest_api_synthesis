21ff2904125c870f0749769496c48ecca41cac79
Zheng Guo <zhg069@eng.ucsd.edu>
2021-01-25 20:05:28 -0800

    init webapp

---
+++ webapp/src/components/collapsibleNode.js
@@ -0,0 +1,38 @@
+import rd3 from 'react-d3-library'
+
+const RD3Component = rd3.Component;
+
+var treeData =
+  {
+    "name": "Top Level",
+    "children": [
+      { 
+        "name": "Level 2: A",
+        "children": [
+          { "name": "Son of A" },
+          { "name": "Daughter of A" }
+        ]
+      },
+      { "name": "Level 2: B" }
+    ]
+  };
+
+class collapsibleNode extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {d3: ''}
+  }
+
+  componentDidMount() {
+    this.setState({d3: node});
+  }
+
+  render() {
+    return (
+      <div>
+        <RD3Component data={this.state.d3} />
+      </div>
+    )
+  }
+};
